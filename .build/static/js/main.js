7b9a7b4ef286d3bbcd700f31cf58085a82b04f09
Sam Tabibzadeh <sam.tzadeh@gmail.com>
2016-07-20 15:10:42 -0400

    Added pages

--- .build/static/js/main.js
+++ .build/static/js/main.js
@@ -1,1 +1,11 @@
-
+$(document).ready(function(){
+    $('#electronic').click(function(){
+        $('#electronicText').toggle();
+    });
+    $('#gSeries').click(function(){
+        $('#gSeriesText').toggle();
+    });
+    $('#iSensivity').click(function(){
+        $('#iSensivityText').toggle();
+    });
+});
