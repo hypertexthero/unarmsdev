18674711c8539910613b637986fca277678db4ca
Simon Griffee <simon@hypertexthero.com>
2016-08-04 13:34:01 -0400

    changes

---
+++ plugins/my_template_tags.py
@@ -0,0 +1,19 @@
+from django import template
+
+def if_current_section(context, link_url, positive=True, negative=False, startswith='false'):
+    """
+    Return one of the passed parameters if the URL passed is the current one.
+    For consistency reasons, we use the link_url of the page.
+    smg - added startswith, if 'true' operate on url startswith instead of whole url.
+    """
+    page = context['__CACTUS_CURRENT_PAGE__']
+    if startswith == 'true':  # smg - mods as above in docstring
+        return positive if page.link_url.startswith(link_url) else negative
+    else:
+        return positive if page.link_url == link_url else negative
+
+def preBuild(site):
+    register = template.Library()
+    register.simple_tag(takes_context=True)(if_current_section)
+    template.base.builtins.append(register)
+
