5a71acc4bb1e23dfd565ee7960ed7964bffc1429
Simon Griffee <simon@hypertexthero.com>
2016-08-08 15:16:48 -0400

    fixed navbar active section and first link, refactored staff ia, faq show/hide js

--- plugins/my_template_tags.py
+++ plugins/my_template_tags.py
@@ -5,6 +5,10 @@ def if_current_section(context, link_url, positive=True, negative=False, startsw
     Return one of the passed parameters if the URL passed is the current one.
     For consistency reasons, we use the link_url of the page.
     smg - added startswith, if 'true' operate on url startswith instead of whole url.
+  
+    Usage: <li class="dropdown {% if_current_section '/photos/' 'active' '' 'true' %}">
+  
+    https://github.com/eudicots/Cactus/issues/164
     """
     page = context['__CACTUS_CURRENT_PAGE__']
     if startswith == 'true':  # smg - mods as above in docstring
